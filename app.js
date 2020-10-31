73743f2dccdd1121539d3ebeebb13ebbeb1cf706
juancaruizc <juancruizcruz@gmail.com>
2020-10-31 12:37:18 -0700

    testing event listener

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
 const nav = document.querySelector('nav');
 const name = document.querySelector('h1');
+const a = document.querySelector('a');
+
+a.addEventListener('click', (e) => {
+  e.style.backgroundColor = 'red';
+});
